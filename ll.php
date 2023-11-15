bc2e58b93b0bf97cd250c96cd029629d43d7dfb9
Maurits <maurits.ruiter@gmail.com>
2023-11-15 11:55:38 +0100

    deleted ll.php test file

--- ll.php
+++
@@ -1,2 +0,0 @@
-<a href="edit.php?edit=<?php echo $row["id"]; ?>" class="btn btn-sm btn-outline-success" style="text-decoration: none !important; border-radius: 0;">Bewerken</a>
-<a href="index.php?delete=<?php echo $row["id"]; ?>" class="btn btn-sm btn-outline-danger" style="text-decoration: none !important; border-radius: 0;" data-bs-toggle="modal" data-bs-target="#confirmDeleteModal<?php echo $row["id"]; ?>">Verwijderen</a>
